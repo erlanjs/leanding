47ae3021b17b4c1af7b0683dad07b33e642f83ea
Erlan Jusupov <erlan@mancho.dev>
2022-10-30 20:27:07 +0600

    atomize config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// External Dependensies
+import { Div, Text } from "atomize";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div className="App">
+      <Text textColor="red">Erlan</Text>
+    </Div>
   );
 }
 
